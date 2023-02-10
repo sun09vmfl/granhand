d748a09f0eaa0eceb5da9e45d676371a710c313c
sun09vmfl <colin_first@naver.com>
2023-02-10 17:56:53 +0900

    json 파일 제작

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
         <Route path="search" element={<Search />} />
         <Route path="login" element={<Login />} />
         <Route path="cart" element={<Cart />} />
-        <Route path="/" element={<ProductList />} />
-        <Route path="products">
+        <Route path="/">
+          <Route index element={<ProductList />} />
           <Route path=":itemId" element={<ProductPage />} />
         </Route>
       </Routes>
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import '../styles/productpage.scss';
 
-function ProductPage(props) {
-  return <div></div>;
+function ProductPage() {
+  return (
+    <>
+      <div className="info">
+        <h1>상품 상세 페이지</h1>
+      </div>
+    </>
+  );
 }
 
 export default ProductPage;
