e843165e1559bc1315fec2e97261dbdae694ab14
sun09vmfl <colin_first@naver.com>
2023-02-13 14:40:10 +0900

    레이아웃 무너짐 수정

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -8,27 +8,30 @@ function ProductList() {
 
   return (
     <div className="main">
-      <div className="topBox">
-        <p className="num">
-          Body <span>14</span>
-        </p>
-        <div className="price">
-          <p>
-            <Link to="/">낮은 가격 순</Link>
-          </p>
-          <p>
-            <Link to="/">높은 가격 순</Link>
-          </p>
-          <p>
-            <Link to="/">판매 순</Link>
-          </p>
-        </div>
-      </div>
       <SideMenu />
-      <div className="main-img">
-        {GranhandItems.map(item => (
-          <ProductItem key={item.id} item={item} />
-        ))}
+      <div className="mainBox">
+        <div className="topBox">
+          <p className="num">
+            Body <span>14</span>
+          </p>
+          <div className="price">
+            <p>
+              <Link to="/">낮은 가격 순</Link>
+            </p>
+            <p>
+              <Link to="/">높은 가격 순</Link>
+            </p>
+            <p>
+              <Link to="/">판매 순</Link>
+            </p>
+          </div>
+        </div>
+
+        <div className="main-img">
+          {GranhandItems.map(item => (
+            <ProductItem key={item.id} item={item} />
+          ))}
+        </div>
       </div>
     </div>
   );
